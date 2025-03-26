dc158c9e493eb3b874f6681a367f22f876166efe
laharikarrotu <laharikarrothu@gmail.com>
2025-03-26 14:01:43 -0400

    build changes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -16,6 +16,7 @@
 
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import './setupTests';
 import App from './App';
 
 test('renders learn react link', () => {
